933036aafa85ba8edd5deb36a1f5e0adb7e4730e
AishwaryaPSU <aishwarya.uvce@gmail.com>
2019-07-12 20:22:41 -0500

    Updates

---
+++ precache-manifest.a623aa1c311a4918b1d3c8e054e9ddfd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333148265e498f460dc583958da44e5f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "af67bae2826170a642dc",
+    "url": "/Portfolio/static/css/main.0c5c9c1e.chunk.css"
+  },
+  {
+    "revision": "015af1e8945680218359",
+    "url": "/Portfolio/static/js/2.c830b613.chunk.js"
+  },
+  {
+    "revision": "af67bae2826170a642dc",
+    "url": "/Portfolio/static/js/main.527a2a35.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/Portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/Portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/Portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/Portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "2ade491269ad01b6538778b30995ae3b",
+    "url": "/Portfolio/static/media/lost-n-found.2ade4912.jpg"
+  },
+  {
+    "revision": "0d24e62229ded8b2170df06881db5555",
+    "url": "/Portfolio/static/media/object_detection_logo.0d24e622.png"
+  },
+  {
+    "revision": "562794fa0242b1680a6bd9b108c4f496",
+    "url": "/Portfolio/static/media/profile_me.562794fa.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/Portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
